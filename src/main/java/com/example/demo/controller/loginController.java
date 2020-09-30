75283a0ac0c05fcdf3e4ddffc666eeea868166ac
tamoto <taku92860110@gmail.com>
2020-09-30 09:49:41 +0900

    login画面作成

---
+++ src/main/java/com/example/demo/controller/loginController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class loginController {
+
+	//ログイン画面遷移
+	@GetMapping("/login")
+	public String login(Model model) {
+		return "login";
+	}
+
+	//ログインボタンポスト用
+	@PostMapping("/login")
+	public String postLogin(Model model) {
+		return "login";
+	}
+
+
+
+}

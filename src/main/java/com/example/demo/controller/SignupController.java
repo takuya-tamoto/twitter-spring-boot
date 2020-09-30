d0c29d98fafe4ae883c26e07acc16df6a76052d0
tamoto <taku92860110@gmail.com>
2020-09-30 10:06:29 +0900

    新規登録画面作成

--- src/main/java/com/example/demo/controller/SignupController.java
+++ src/main/java/com/example/demo/controller/SignupController.java
@@ -1,5 +1,23 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class SignupController {
+	
+	//新規登録画面遷移
+	@GetMapping("/signup")
+	public String signUp(Model model) {
+		return "signup";
+	}
+	
+	//ユーザー情報をPOST
+	@PostMapping("/signup")
+	public String postSignUp(Model model) {
+		return "redirect:/login"; 
+	}
 
 }
